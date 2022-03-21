5576f07dac773596088cc3fe7bfc0f88dfb42701
devjo0810 <devjo0810@gmail.com>
2022-03-21 22:16:46 +0900

    eslint 설정, evn 설정, @경로 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
   root: true,
+  parserOptions: {
+      parser: 'babel-eslint'
+  },
   env: {
-    node: true
+      browser: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+      'plugin:vue/essential',
+      'standard'
   ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
+  plugins: [
+      'vue'
+  ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+      // allow async-await
+      'generator-star-spacing': 'off',
+      // allow debugger during development
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
